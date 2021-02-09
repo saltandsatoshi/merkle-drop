3ef2738dced0ed586aa6825819058ccacea13787
Carl Farterson <carlfarterson@gmail.com>
2021-02-09 10:26:58 -0800

    Fix: add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,21 @@
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-web3";
 
-require("dotenv").config();
+module.exports = {
+    solidity: {
+        compilers: [
+            {version: "0.7.4"}
+        ]
+    },
+    networks: {
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+            accounts: [`${process.env.PRIVATE_KEY}`,]
+        },
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+            accounts: [`${process.env.PRIVATE_KEY}`,]
+        }
+    }
+}
