ce7df0bdf0866c95ee63c2e7955df48a5cb35857
Carl Farterson <carlfarterson@gmail.com>
2021-02-10 12:29:52 -0800

    Add rinkeby connector

--- frontend/src/connectors.ts
+++ frontend/src/connectors.ts
@@ -6,6 +6,7 @@ import { TrezorConnector } from '@web3-react/trezor-connector'
 export const INFURA_JSON_RPC_API = {
   1: `https://mainnet.infura.io/v3/663207cc32894545b804fbc519d9fa2a`,
   42: `https://kovan.infura.io/v3/663207cc32894545b804fbc519d9fa2a`,
+  4: `https://rinkeby.infura.io/v3/663207cc32894545b804fbc519d9fa2a`,
 }
 const POLLING_INTERVAL = 12000
 
@@ -13,7 +14,7 @@ export const injected = new InjectedConnector({ supportedChainIds: [1, 42] })
 
 export const walletconnect = new WalletConnectConnector({
   rpc: { 
-    1: INFURA_JSON_RPC_API[1],
+    4: INFURA_JSON_RPC_API[4],
   },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
@@ -22,14 +23,14 @@ export const walletconnect = new WalletConnectConnector({
 
 
 export const ledger = new LedgerConnector({
-  chainId: 1,
-  url: INFURA_JSON_RPC_API[1],
+  chainId: 4,
+  url: INFURA_JSON_RPC_API[4],
   pollingInterval: POLLING_INTERVAL
 })
 
 export const trezor = new TrezorConnector({
-  chainId: 1,
-  url: INFURA_JSON_RPC_API[1],
+  chainId: 4,
+  url: INFURA_JSON_RPC_API[4],
   pollingInterval: POLLING_INTERVAL,
   manifestEmail: 'dummy@abc.xyz',
   manifestAppUrl: 'http://localhost:1234'
