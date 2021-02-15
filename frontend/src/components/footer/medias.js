1382ca8d8725ee8324436c0f20e7da43e7a73b7b
Carl Farterson <carlfarterson@gmail.com>
2021-02-15 07:36:38 -0800

    Fix: Add airdrop to medias

--- frontend/src/components/footer/medias.js
+++ frontend/src/components/footer/medias.js
@@ -1,9 +1,11 @@
 // import Coingecko from "../../assets/logos/coingecko.webp";
 import Etherscan from "../../assets/logos/etherscan.webp";
+// TODO: create new image for airdrop
+import Airdrop from "../../assets/logos/salty-bg.webp";
 
 require("dotenv").config();
 
 export const medias = [
-  [ Etherscan, "Etherscan", `https://etherscan.io/token/${process.env.SALTY_ADDRESS}`]
-  // [ Coingecko, "coingecko", "https://www.coingecko.com/en/coins/TODO" ],
+  [ Etherscan, "$SALTY (Etherscan)", `https://etherscan.io/token/${process.env.SALTY_ADDRESS}`],
+  [ Airdrop, "Community $SALTY Airdrop", "https://claim.saltandsatoshi.com"]
 ];
